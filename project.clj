3f2162cc6be8e358133077b990d66bd2977b366c
Dan Davison <dandavison7@gmail.com>
2015-12-25 16:38:56 +0000

    dev config
    Reload and stacktrace
    
    https://mmcgrana.github.io/2010/03/clojure-web-development-ring.html

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [ring/ring-core "1.4.0"]
-                 [ring/ring-jetty-adapter "1.4.0"]]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [ring/ring-devel "1.4.0"]]
+  ;; :dev-dependencies [[ring/ring-devel "1.4.0"]]
   :main ^:skip-aot bookshelf.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/bookshelf/core.clj
+++ src/bookshelf/core.clj
@@ -1,5 +1,8 @@
 (ns bookshelf.core
-  (:gen-class))
+  (:gen-class)
+  (:use ring.adapter.jetty)
+  (:use ring.middleware.reload)
+  (:use ring.middleware.stacktrace))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -10,3 +13,11 @@
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body "Hello World"})
+
+(def app
+  (-> #'handler
+      (wrap-reload '(bookshelf.core))
+      (wrap-stacktrace)))
+
+(defn run []
+  (run-jetty #'app {:port 3000}))
