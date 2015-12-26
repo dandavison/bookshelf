24f621c04910f2af26cad826ea88a749987a9254
Dan Davison <dandavison7@gmail.com>
2015-12-26 08:56:11 +0000

    Revert "Check defroutes behavior"
    This reverts commit 8de01cb8bd444db822d91c07e74ed9653e63de42.

--- src/bookshelf/core.clj
+++ src/bookshelf/core.clj
@@ -7,17 +7,11 @@
   (:use [compojure.core :refer :all])
   (:use [compojure.route :as route]))
 
-(defn write-to-file []
-  (with-open [w (clojure.java.io/writer "/tmp/out" :append true)]
-    (.write w "hello\n"))
-  "wrote to file")
-
 (defn library-files []
   (file-seq (clojure.java.io/file "/Users/dan/GoogleDrive/Literature/iPad")))
 
 (defroutes handler
   (GET "/" [] (str/join "<br>" (library-files)))
-  (GET "/write" [] (write-to-file))
   (route/not-found "Invalid request"))
 
 (def app
