cf6d358d2e9b6fc5af5b0faf0b9b268a88725dbc
Dan Davison <dandavison7@gmail.com>
2015-12-26 00:26:22 +0000

    List files

--- src/bookshelf/core.clj
+++ src/bookshelf/core.clj
@@ -1,5 +1,6 @@
 (ns bookshelf.core
   (:gen-class)
+  (:use [clojure.string :as str])
   (:use [ring.adapter.jetty :as jetty])
   (:use [ring.middleware.reload :as reload])
   (:use [ring.middleware.stacktrace :as stacktrace])
@@ -11,8 +12,11 @@
     (.write w "hello\n"))
   "wrote to file")
 
+(defn library-files []
+  (file-seq (clojure.java.io/file "/Users/dan/GoogleDrive/Literature/iPad")))
+
 (defroutes handler
-  (GET "/" [] "Hello World")
+  (GET "/" [] (str/join "<br>" (library-files)))
   (GET "/write" [] (write-to-file))
   (route/not-found "Invalid request"))
 
