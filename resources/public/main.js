62aa0005acf2b9cde38427a0a2a0620bf56794fd
Dan Davison <dandavison7@gmail.com>
2015-12-27 12:21:39 +0000

    Fetch data from server

--- resources/public/main.js
+++ resources/public/main.js
@@ -1,27 +1,64 @@
-var BookTable = React.createClass({
+var TableRow = React.createClass({
   render: function() {
+    return (
+      <tr>
+        <td>{this.props.row.name}</td>
+        <td>{this.props.row.size}</td>
+      </tr>
+    )
+  }
+})
+
+var Table = React.createClass({
+  render: function() {
+    var rows = [];
+    this.state.rows.forEach(function(row){
+      rows.push(<TableRow key={row.hash} row={row} />)
+    })
     return (
       <table>
         <thead>
           <tr>
             <td>Name</td>
             <td>Size</td>
-            <td>Notes</td>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
+          {rows}
         </tbody>
       </table>
     )
+  },
+  getInitialState: function() {
+    return {
+      rows: [],
+      message: ''
+    }
+  },
+  componentDidMount: function() {
+    this.fetchData();
+  },
+  fetchData: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      // cache: false,
+      success: function(data) {
+        this.setState({
+          rows: data
+        });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+        this.setState({
+          message: err.toString()
+        });
+      }.bind(this)
+    })
   }
 })
 
 ReactDOM.render(
-  <BookTable />,
+  <Table rows={[]} url='/json/' />,
   document.getElementById('content')
 );
