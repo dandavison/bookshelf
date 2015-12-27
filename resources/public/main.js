b3112f82b06e4baf2207d0512ce01e3d00505248
Dan Davison <dandavison7@gmail.com>
2015-12-27 10:59:30 +0000

    Table component

--- resources/public/main.js
+++ resources/public/main.js
@@ -1,4 +1,27 @@
+var BookTable = React.createClass({
+  render: function() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Size</td>
+            <td>Notes</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    )
+  }
+})
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <BookTable />,
   document.getElementById('content')
 );
